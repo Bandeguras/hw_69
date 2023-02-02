4c5a4d9328d793d51edbb6c4daefe72272c64329
Said Saliev <repsaid@gmail.com>
2023-02-02 15:15:28 +0600

    первый этап

--- source/api_v1/urls.py
+++ source/api_v1/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path, include
 
-from api_v1.views import echo_view
+from api_v1.views import echo_view, get_token_view
 app_name = 'api_v1'
 
-# article_url = [
-#     path('', article_view)
-# ]
-
 urlpatterns = [
-    path('echo/', echo_view),
+    path('<str:method>/', echo_view),
+    path('get_token/', get_token_view),
 ]
--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,11 +1,43 @@
-from django.http import HttpResponse
-from datetime import datetime
+from functools import reduce
+from django.http import HttpResponse, HttpResponseNotAllowed
+from django.views.decorators.csrf import ensure_csrf_cookie
+import json
 
 
 def echo_view(request, *args, **kwargs):
-    response_data = {
-        'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-        'method': request.method
-    }
-    response = HttpResponse(response_data)
+    user_value = {}
+
+    if request.body:
+        user_value = json.loads(request.body)
+
+    answer = {}
+    try:
+        if kwargs['method'] == 'add':
+            answer['answer'] = sum(user_value.values())
+
+        elif kwargs['method'] == 'subtract':
+            answer['answer'] = reduce(lambda x, y:x-y, user_value.values())
+
+        elif kwargs['method'] == 'multiply':
+            answer['answer'] = reduce(lambda x, y:x*y, user_value.values())
+
+        elif kwargs['method'] == 'divide':
+            try:
+                answer['answer'] = reduce(lambda x, y:x/y, user_value.values())
+            except ZeroDivisionError:
+                answer['answer'] = "Division by zero!"
+
+    except TypeError:
+        answer['answer'] = "Value must be integer"
+
+    response_data_json = json.dumps(answer)
+    response = HttpResponse(response_data_json)
+    response['Content-Type'] = 'application/json'
     return response
+
+
+@ensure_csrf_cookie
+def get_token_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        return HttpResponse()
+    return HttpResponseNotAllowed('Only GET request are allowed')
