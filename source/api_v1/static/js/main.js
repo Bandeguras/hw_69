2071103d86e10750687ae86eb3e286f4cd66333b
Said Saliev <repsaid@gmail.com>
2023-02-03 17:36:52 +0600

    исправил ошибки сделал 2 этап

--- source/api_v1/static/js/main.js
+++ source/api_v1/static/js/main.js
@@ -1,6 +1,57 @@
-function Said(){
-    let a = document.getElementById('value1').value
-    let b = document.getElementById('value2').value
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    function onButtonClickClose() {
+        let div2 = document.getElementById('note');
+        let child = div2.children[0];
+        div2.removeChild(child);
+    }
 
-    console.log(a)
-}
+    async function buttonClick(event) {
+            let target = event.target;
+            let url =  target.dataset['indexLink'];
+            let a = document.getElementById('first').value;
+            let b = document.getElementById('second').value;
+
+            let response = await fetch(url,{
+                    method: 'POST',
+                    headers: {"X-CSRFToken": getCookie('csrftoken'),
+                             "content-type": "application/json"},
+                    body:JSON.stringify({a, b}),
+            });
+
+            if(response.ok) {
+                let index_text = await response.json();
+                let div = document.createElement('div')
+                div.className='alert';
+                div.innerHTML= `<p>Answer is : ${index_text['answer']}</p><button class='button'  onclick='onButtonClickClose()'><i class='bi bi-x-circle'></i></button>`;
+                let div2 = document.getElementById('note')
+                div2.appendChild(div)
+                setTimeout(onButtonClickClose,5000)
+            }
+            else {
+
+            }
+    }
+
+    async function onLoad(){
+        const button = document.querySelectorAll(`[id="click_me"]`);
+        if(button){
+        for(let i of button){
+            i.onclick = buttonClick;
+        }
+        }
+    }
+
+    window.addEventListener('load', onLoad)
--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,6 +1,5 @@
 from functools import reduce
-from django.http import HttpResponse, HttpResponseNotAllowed
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 import json
 
@@ -8,35 +7,42 @@ from django.views.generic import TemplateView
 
 
 def echo_view(request, *args, **kwargs):
-    user_value = {}
-
-    if request.body:
-        user_value = json.loads(request.body)
-
-    answer = {}
-    try:
-        if kwargs['method'] == 'add':
-            answer['answer'] = sum(user_value.values())
-
-        elif kwargs['method'] == 'subtract':
-            answer['answer'] = reduce(lambda x, y:x-y, user_value.values())
-
-        elif kwargs['method'] == 'multiply':
-            answer['answer'] = reduce(lambda x, y:x*y, user_value.values())
-
-        elif kwargs['method'] == 'divide':
+    if request.method == 'POST':
+        if request.body:
+            user_value = json.loads(request.body)
+            answer = {}
             try:
-                answer['answer'] = reduce(lambda x, y:x/y, user_value.values())
-            except ZeroDivisionError:
-                answer['answer'] = "Division by zero!"
+                for key in user_value:
+                    user_value[key] = int(user_value[key])
 
-    except TypeError:
-        answer['answer'] = "Value must be integer"
+                if kwargs['method'] == 'add':
+                    answer['answer'] = sum(user_value.values())
 
-    response_data_json = json.dumps(answer)
-    response = HttpResponse(response_data_json)
-    response['Content-Type'] = 'application/json'
-    return response
+                elif kwargs['method'] == 'subtract':
+                    answer['answer'] = reduce(lambda x, y:x-y, user_value.values())
+
+                elif kwargs['method'] == 'multiply':
+                    answer['answer'] = reduce(lambda x, y:x*y, user_value.values())
+
+                elif kwargs['method'] == 'divide':
+                    try:
+                        answer['answer'] = reduce(lambda x, y:x/y, user_value.values())
+                    except ZeroDivisionError:
+                        answer['answer'] = "Division by zero!"
+            except TypeError:
+                answer['answer'] = "Value must be integer"
+
+            except ValueError:
+                answer['answer'] = "Value must be integer"
+
+            response_data_json = json.dumps(answer)
+            response = HttpResponse(response_data_json)
+            response['Content-Type'] = 'application/json'
+            return response
+    else:
+        response = JsonResponse({'error': 'No data provided!'})
+        response.status_code = 400
+        return response
 
 
 @ensure_csrf_cookie
